704f06b7b98e6cb1fac208feeca10e51720b48c8
Jan Lee <janlee@Jans-MacBook-Pro.local>
2020-04-14 11:32:25 -0500

    Updates

---
+++ precache-manifest.29bde313092c4e59137810c765ba674b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01840f0eb73a8e8fbeb6e446e9c53c72",
+    "url": "/jlee8020/project-4-assessment.git/index.html"
+  },
+  {
+    "revision": "f0107c39228bcc5eeed5",
+    "url": "/jlee8020/project-4-assessment.git/static/css/main.2ddb3bd6.chunk.css"
+  },
+  {
+    "revision": "bf9faed5984ec60f7781",
+    "url": "/jlee8020/project-4-assessment.git/static/js/2.36c0ca1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jlee8020/project-4-assessment.git/static/js/2.36c0ca1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0107c39228bcc5eeed5",
+    "url": "/jlee8020/project-4-assessment.git/static/js/main.d7e36fc8.chunk.js"
+  },
+  {
+    "revision": "1083fa3fc3ea97cbf13e",
+    "url": "/jlee8020/project-4-assessment.git/static/js/runtime-main.ef407be4.js"
+  }
+]);
